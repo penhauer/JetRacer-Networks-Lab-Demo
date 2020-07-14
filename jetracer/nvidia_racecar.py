a9890ec2cff76ce7a8b0668dec2adcd0680bb309
waveshare <36142105+waveshare@users.noreply.github.com>
2020-07-14 20:12:52 +0800

    Update nvidia_racecar.py

--- jetracer/nvidia_racecar.py
+++ jetracer/nvidia_racecar.py
@@ -5,19 +5,18 @@ from adafruit_servokit import ServoKit
 
 class NvidiaRacecar(Racecar):
     
-    i2c_address1 = traitlets.Integer(default_value=0x40)
-    i2c_address2 = traitlets.Integer(default_value=0x60)
+    i2c_address = traitlets.Integer(default_value=0x40)
     steering_gain = traitlets.Float(default_value=-0.65)
     steering_offset = traitlets.Float(default_value=0)
     steering_channel = traitlets.Integer(default_value=0)
     throttle_gain = traitlets.Float(default_value=0.8)
+    throttle_channel = traitlets.Integer(default_value=1)
     
     def __init__(self, *args, **kwargs):
         super(NvidiaRacecar, self).__init__(*args, **kwargs)
-        self.kit = ServoKit(channels=16, address=self.i2c_address1)
-        self.motor = ServoKit(channels=16, address=self.i2c_address2)
-        self.motor._pca.frequency = 1600
+        self.kit = ServoKit(channels=16, address=self.i2c_address)
         self.steering_motor = self.kit.continuous_servo[self.steering_channel]
+        self.throttle_motor = self.kit.continuous_servo[self.throttle_channel]
     
     @traitlets.observe('steering')
     def _on_steering(self, change):
